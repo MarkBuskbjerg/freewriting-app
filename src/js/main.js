bce4dacfad08b1fccafbe72be6de98fa40bff5ad
Mark Buskbjerg <mark.buskbjerg@gmail.com>
2018-11-10 01:04:38 +0100

    Made it render from JS + start on first keyup event + lockdown textarea after time is up

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,113 @@
-var startTimerButton = document.getElementById('start');
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  setInterval(function() {
-    minutes = parseInt(timer / 60, 10)
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      display.textContent = "00:00";
-    }
-  }, 1000);
-}
-
-var typeWatcher = function(text, timer) {
-  var time = timer;
-  setInterval(function() {
-    var content = text.value;
-    text.addEventListener('keyup', function(e) {
-      time = timer;
+//
+// Helper Methods
+//
+/**
+ * Render a template into the DOM
+ * @param  {String|Function} template The template to render
+ * @param  {String|Node}     elem     The element to render into
+ * @return {[type]}                   The element
+ */
+var render = function(template, elem) {
+  // Set rendering element for the component
+  if (typeof template === 'function') {
+    template.elem = elem;
+  }
+  // If elem is an element, use it.
+  // If it's a selector, get it.
+  elem = typeof elem === 'string' ? document.querySelector(elem) : elem;
+  if (!elem) return;
+  // Get the template
+  template = (typeof template === 'function' ? template(template.state) : template);
+  if (typeof template !== 'string') return;
+  // Render the template into the element
+  if (elem.innerHTML === template) return;
+  elem.innerHTML = template;
+  // Dispatch a render event
+  if (typeof window.CustomEvent === 'function') {
+    var event = new CustomEvent('render', {
+      bubbles: true
     });
-    if (time <= 0) {
-      var deleteText = content.split('');
-      deleteText.pop();
-      var newText = deleteText.join('');
-      text.value = newText;
-    } else {
-      time -= 500;
-    }
-  }, 500);
-}
+    elem.dispatchEvent(event);
+  }
+  // Return the elem for use elsewhere
+  return elem;
+};
 
+var freewriting = (function() {
+  'use strict;';
 
-startTimerButton.addEventListener('click', function(e) {
-  var toolbar = document.getElementById('toolbar');
+  // Public APIs
+  var publicAPIs = {};
 
-  var workPeriodInMinutes = 10;
-  var workPeriodInSeconds = 60 * workPeriodInMinutes;
-  var display = document.querySelector('#timer');
-  var textarea = document.getElementById('textInput');
-  startTimerButton.id = 'stop';
-  startTimerButton.textContent = 'Stop legen';
-  toolbar.removeChild(startTimerButton);
-  startTimer(workPeriodInSeconds, display);
-  typeWatcher(textarea, 1000);
-}, false);
+  publicAPIs.deleteText = function(text) {
+    var deleteText = text.split('');
+    deleteText.pop();
+    return deleteText.join('');
+  };
+
+  publicAPIs.startTimer = function(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function() {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+      if (timer === 0) {
+        var textContainer = document.getElementById('textInput');
+        var placeholder = document.getElementById('placeholder');
+        var template = '<div class="tekstr-info-box"><p><strong>Godt arbejdet. Her er din tekst:</strong></p><p>' + textContainer.value + '</p></div>';
+        textContainer.style.display = 'none';
+        placeholder.innerHTML = template;
+      }
+      if (--timer < 0) {
+        display.textContent = "00:00";
+      }
+    }, 1000);
+  };
+
+  publicAPIs.typeWatcher = function(textContainer, timer) {
+    var time = timer;
+    setInterval(function() {
+      var content = textContainer.value;
+      textContainer.addEventListener('keyup', function(e) {
+        time = timer;
+      });
+      if (time <= 0) {
+        textContainer.value = publicAPIs.deleteText(content);
+      } else {
+        time -= 500;
+      }
+    }, 500);
+  };
+
+  // Return our public APIs
+  return publicAPIs;
+})();
+
+var template = '<div class="container" id ="toolbar">' + 'Tid: <span id="timer">10:00</div>' + '</div>' + '<div class="container">' + '<textarea id="textInput" class="textarea"></textarea>' + '<div id="placeholder"></div>' + '</div>';
+console.log(template);
+render(template, '#app');
+
+/*
+A simple function to start everything off
+*/
+var initiator = function(e) {
+  // If keyup is inside of the the textarea
+  if (e.target.id === 'textInput') {
+    // Make sure that the freewriting exercise is only handled once
+    document.removeEventListener('keyup', initiator, false);
+
+    // This is basically just settings for the timer. Could be fun to make this adjustable
+    var workPeriodInSeconds = 60 * 10;
+
+    var textarea = document.getElementById('textInput');
+    var display = document.getElementById('timer');
+    freewriting.startTimer(workPeriodInSeconds, display);
+    freewriting.typeWatcher(textarea, 2000);
+  }
+};
+// Listen for keyup events across the document
+document.addEventListener('keyup', initiator, false);
